1e33c5df64f656646eb5e6e27289cd87de4a79e9
langerdaniel9 <71530426+langerdaniel9@users.noreply.github.com>
2022-04-27 01:20:06 -0400

    Update Sum and Difference of Two Numbers.c

--- C/Sum and Difference of Two Numbers.c
+++ C/Sum and Difference of Two Numbers.c
@@ -1,3 +1,15 @@
+/*
+Task
+
+Your task is to take two numbers of int data type, two numbers of float data type as input and output their sum:
+
+1. Declare 4 variables: two of type int and two of type float.
+2. Read 2 lines of input from stdin (according to the sequence given in the 'Input Format' section below) and initialize your 4 variables.
+3. Use the + and - operator to perform the following operations:
+    - Print the sum and difference of two int variable on a new line.
+    - Print the sum and difference of two float variable rounded to one decimal place on a new line.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
