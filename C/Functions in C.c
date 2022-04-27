e06b1381e15d69d3e362d8cf190a7a88ec328909
langerdaniel9 <71530426+langerdaniel9@users.noreply.github.com>
2022-04-27 01:14:27 -0400

    Create Functions in C.c

---
+++ C/Functions in C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d) {
+    int list[] = {a, b, c, d};
+    int largest = d;
+    
+    for(int i=0; i<3; i++) {
+        if(list[i]>largest) {
+            largest = list[i];
+        }
+    }
+    
+    return largest;
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
