e664f0b1582ff3cc358ba1f9e6ac042c7015c2f7
langerdaniel9 <71530426+langerdaniel9@users.noreply.github.com>
2022-04-27 01:18:25 -0400

    Update "Hello World!" in C.c

--- C/"Hello World!" in C.c
+++ C/"Hello World!" in C.c
@@ -16,6 +16,5 @@ int main()
     scanf("%[^\n]%*c", &s);
   	
     printf("Hello, World!\n%s", s);
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
